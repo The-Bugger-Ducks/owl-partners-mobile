224e8b16dd4b6afa715a9756a88d1f35fb728ec6
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-03-23 15:22:36 -0300

    feat: create input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const TextInput = styled.TextInput`
+  background: #f4f5f7;
+  padding: 0px 16px;
+  border-radius: 8px;
+  border: 1px solid #cccccc;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+  min-width: 85%;
+`;
+
+export const InputContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const IconButton = styled.TouchableOpacity`
+  width: 48px;
+  align-items: center;
+  justify-content: center;
+`;
