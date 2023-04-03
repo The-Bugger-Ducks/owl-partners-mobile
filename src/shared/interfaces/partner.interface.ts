bf2d5abece4f1cb3a0348bb2178e1ff572c49baa
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-04-02 22:52:48 -0300

    feat: separate partnerships by activity

--- src/shared/interfaces/partner.interface.ts
+++ src/shared/interfaces/partner.interface.ts
@@ -1,4 +1,5 @@
 export interface IPartnership {
+  id?: string;
   name: string;
   email: string;
   phoneNumber: string;
--- src/shared/services/partner.request.ts
+++ src/shared/services/partner.request.ts
@@ -2,9 +2,11 @@ import { PARTNERSHIP_ENDPOINTS } from "../utils/endpoints";
 import { api } from "./api";
 
 class PartnerRequest {
-  public async List() {
+  public async List(disabled: boolean) {
     try {
-      const response = await api.get(PARTNERSHIP_ENDPOINTS.LIST_PARTNERSHIP);
+      const response = await api.get(
+        PARTNERSHIP_ENDPOINTS.LIST_PARTNERSHIP + "?disabled=" + disabled,
+      );
       return response.data;
     } catch (error) {
       console.error(error);
