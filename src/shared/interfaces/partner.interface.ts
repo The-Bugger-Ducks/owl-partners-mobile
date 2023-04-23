b3b592bb542fd8a6e2ff7eb63d0dff0475268614
thhenrique <henriquethiago298@gmail.com>
2023-04-23 20:57:44 -0300

    create methods for formating status partner

--- src/shared/interfaces/partner.interface.ts
+++ src/shared/interfaces/partner.interface.ts
@@ -40,3 +40,18 @@ export interface IModalPropsForm {
   onClose: () => void;
   closeAfterUpdate: () => void;
 }
+
+export enum PartnerStatus {
+  EmProspeccao = "Em prospecção",
+  PrimeiroContatoFeito = "Primeiro contato feito",
+  PrimeiraReuniaoMarcadaRealizada = "Primeira reunião marcada/realizada",
+  DocumentacaoEnviadaEmAnalise_Parceiro = "Documentação enviada/em análise (Parceiro)",
+  DocumentacaoDevolvida_EmAnaliseAcademy = "Documentação devolvida (Academy)",
+  DocumentacaoDevolvida_EmAnaliseLegal = "Documentação devolvida (Legal)",
+  DocumentacaoAnalisadaDevolvida_Parceiro = "Documentação Analisada e devolvida (Parceiro)",
+  EmPreparacaoDeExecutiveSummary_Academy = "Preparação de Executive Sumary (Academy)",
+  ESEmAnalise_Legal = "ES em analise (Legal)",
+  ESEmAnaliseAcademy_Global = "ES em analise (Academy Global)",
+  ProntoParaAssinatura = "Pronto para assinatura",
+  ParceriaFirmada = "Parceria Firmada",
+}
